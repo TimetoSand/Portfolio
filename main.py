cc73202ddb0912484aecd3037f72569eba25615b
Elrond <ozturknuri8qgmail.com>
2022-05-13 07:36:51 -0700

    Add requirements.txt

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import smtplib
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 Bootstrap(app)
 
 MY_EMAIL = os.environ['MY_EMAIL']
